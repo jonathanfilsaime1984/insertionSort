33e20ce52ca4db7e960090055679e39c6bae2ad1
jonathan <jxf131930@utdallas.edu>
2016-01-14 19:46:23 -0600

    the algorithm is working

---
+++ src/com/company/Main.java
@@ -0,0 +1,26 @@
+package com.company;
+
+import java.util.ArrayList;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int[] a = {1,2,3,4,6,7,8,9,5,12,10,13,50,45,32,100};
+
+
+        for(int i = 0 ; i < a.length ; i++){
+            int key = a[i];
+            int j = i - 1;
+            while(j >= 0 && a[j] >= key){
+                a[ j + 1 ] = a[j];
+                j = j - 1;
+                a[ j + 1 ] = key;
+            }
+        }
+
+        for(int i = 0; i < a.length; i++){
+            System.out.print(a[i] + ",");
+        }
+    }
+}
